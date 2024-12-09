3c0000165b416e7bf7ce84a6587e77fa53015fc5
Lerolldesign <iam@lerolldesign.com>
2024-12-09 10:38:39 +0100

    studio

--- storefront/src/app/not-found.tsx
+++ storefront/src/app/not-found.tsx
@@ -1,7 +1,6 @@
-import { ArrowUpRightMini } from "@medusajs/icons"
-import { Text } from "@medusajs/ui"
+import Button from "@modules/elements/button"
+import Ornements from "@modules/layout/templates/ornements"
 import { Metadata } from "next"
-import Link from "next/link"
 
 export const metadata: Metadata = {
   title: "404",
@@ -11,20 +10,18 @@ export const metadata: Metadata = {
 export default function NotFound() {
   return (
     <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-64px)]">
-      <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
-      <p className="text-small-regular text-ui-fg-base">
-        The page you tried to access does not exist.
+      <Ornements />
+      <h1 className="text-[7rem] medium:text-[9rem] xlarge:text-[11rem] 2xlarge:text-[15rem] font-lune text-lune">
+        404
+      </h1>
+      <p className="text-[.9rem] lg:text-[1.1rem] pb-10">
+        Cette page n&apos;exite plus ou á été déplaçée
       </p>
-      <Link
-        className="flex gap-x-1 items-center group"
-        href="/"
-      >
-        <Text className="text-ui-fg-interactive">Go to frontpage</Text>
-        <ArrowUpRightMini
-          className="group-hover:rotate-45 ease-in-out duration-150"
-          color="var(--fg-interactive)"
-        />
-      </Link>
+      <span className="px-10">
+        <Button href="/" variant="primary">
+          Revenir á l&apos;accueil +
+        </Button>
+      </span>
     </div>
   )
 }
--- storefront/src/sanity/structure.ts
+++ storefront/src/sanity/structure.ts
@@ -1,7 +1,17 @@
-import type {StructureResolver} from 'sanity/structure'
+// ./deskStructure.js
+import { CogIcon } from "@sanity/icons"
 
-// https://www.sanity.io/docs/structure-builder-cheat-sheet
-export const structure: StructureResolver = (S) =>
+export const structure = (S: any) =>
   S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+    .title("Website Content")
+    .items([
+      ...S.documentTypeListItems().filter(
+        // Remove the "assist.instruction.context" and "settings" content  from the list of content types
+        (listItem: any) =>
+          !["settings", "assist.instruction.context"].includes(listItem.getId())
+      ),
+      S.listItem()
+        .title("Site Settings")
+        .child(S.document().schemaType("settings").documentId("siteSettings"))
+        .icon(CogIcon),
+    ])
