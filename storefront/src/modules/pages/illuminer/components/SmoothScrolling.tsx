d3f28e6cf21f79528ce8c8ca48ab69ca5bf9c516
Lerolldesign <iam@lerolldesign.com>
2024-12-11 11:56:46 +0100

    lenis smooth

--- storefront/src/modules/pages/illuminer/components/SmoothScrolling.tsx
+++ storefront/src/modules/pages/illuminer/components/SmoothScrolling.tsx
@@ -1,13 +1,31 @@
-"use client";
-import { ReactNode } from 'react';
-import { ReactLenis } from "@studio-freight/react-lenis";
+"use client"
+import Lenis from "@studio-freight/lenis"
+import { ReactNode, forwardRef, useEffect } from "react"
 
-function SmoothScrolling({ children }: { children: ReactNode }) {
-  return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.5 }}>
-      {children}
-    </ReactLenis>
-  );
-}
+const SmoothScrolling = forwardRef<HTMLDivElement, { children: ReactNode }>(
+  ({ children }, ref) => {
+    useEffect(() => {
+      const lenis = new Lenis({
+        lerp: 0.1,
+        duration: 1.5,
+      })
 
-export default SmoothScrolling;
+      function raf(time: number) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+      }
+
+      requestAnimationFrame(raf)
+
+      return () => {
+        lenis.destroy()
+      }
+    }, [])
+
+    return <div ref={ref}>{children}</div>
+  }
+)
+
+SmoothScrolling.displayName = "SmoothScrolling"
+
+export default SmoothScrolling
