9d50a3a9b8c14bad47c46bd7408d8293b420a366
Lerolldesign <iam@lerolldesign.com>
2024-12-12 08:39:58 +0100

    image carousel

--- storefront/src/modules/products/templates/index.tsx
+++ storefront/src/modules/products/templates/index.tsx
@@ -1,5 +1,7 @@
 import React, { Suspense } from "react"
 
+import { HttpTypes } from "@medusajs/types"
+import ImageCarousel from "@modules/products/components/image-carousel"
 import ImageGallery from "@modules/products/components/image-gallery"
 import ProductActions from "@modules/products/components/product-actions"
 import ProductOnboardingCta from "@modules/products/components/product-onboarding-cta"
@@ -9,7 +11,6 @@ import ProductInfo from "@modules/products/templates/product-info"
 import SkeletonRelatedProducts from "@modules/skeletons/templates/skeleton-related-products"
 import { notFound } from "next/navigation"
 import ProductActionsWrapper from "./product-actions-wrapper"
-import { HttpTypes } from "@medusajs/types"
 
 type ProductTemplateProps = {
   product: HttpTypes.StoreProduct
@@ -28,7 +29,7 @@ const ProductTemplate: React.FC<ProductTemplateProps> = ({
 
   return (
     <>
-      <div
+      {/**     <div
         className="content-container flex flex-col small:flex-row small:items-start py-6 relative"
         data-testid="product-container"
       >
@@ -40,6 +41,67 @@ const ProductTemplate: React.FC<ProductTemplateProps> = ({
           <ImageGallery images={product?.images || []} />
         </div>
         <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-12">
+          <ProductOnboardingCta />
+          <Suspense
+            fallback={
+              <ProductActions
+                disabled={true}
+                product={product}
+                region={region}
+              />
+            }
+          >
+            <ProductActionsWrapper id={product.id} region={region} />
+          </Suspense>
+        </div>
+      </div>
+      <div
+        className="content-container my-16 small:my-32"
+        data-testid="related-products-container"
+      >
+        <Suspense fallback={<SkeletonRelatedProducts />}>
+          <RelatedProducts product={product} countryCode={countryCode} />
+        </Suspense>
+      </div>*/}
+      <div
+        className="flex flex-col  medium:flex-row medium:items-start py-6 relative w-full md:px-20 px-3"
+        data-testid="product-container"
+      >
+        <div className="flex flex-col  xl:hidden px-8 mt-32 md:mt-20  ">
+          <div className="md:px-0 block xl:hidden w-full relative">
+            <ImageCarousel images={product?.images || []} />
+          </div>
+
+          <div className="order-last px-1">
+            <div className="flex flex-col small:sticky small:top-[20vh] small:py-0 medium:max-w-[300px] w-full py-8 gap-y-6">
+              <ProductInfo product={product} />
+              <div className="flex flex-col small:sticky small:top-48 small:py-0 medium:max-w-[300px] w-full py-8 gap-y-12 px-5">
+                <ProductOnboardingCta />
+                <Suspense
+                  fallback={
+                    <ProductActions
+                      disabled={true}
+                      product={product}
+                      region={region}
+                    />
+                  }
+                >
+                  <ProductActionsWrapper id={product.id} region={region} />
+                </Suspense>
+              </div>
+              <ProductTabs product={product} />
+            </div>
+          </div>
+        </div>
+        <div className="hidden md:hidden xl:flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-6">
+          <ProductInfo product={product} />
+          <ProductTabs product={product} />
+        </div>
+        <div className="hidden px-8 md:px-0 xl:block w-full relative">
+          <ImageGallery images={product?.images || []} />
+        </div>
+
+        <div className="hidden xl:flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-12 px-5">
           <ProductOnboardingCta />
           <Suspense
             fallback={
