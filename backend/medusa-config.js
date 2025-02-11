ed844c070d24bb18b5dd9f576ff85f83dd253adb
Lerolldesign <iam@lerolldesign.com>
2025-02-11 12:53:19 +0100

    providers

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -47,34 +47,36 @@ const medusaConfig = {
       resolve: "@medusajs/medusa/notification",
       options: {
         providers: [
-          // ...
-          {
-            resolve: "@medusajs/medusa/notification-sendgrid",
-            id: "sendgrid",
-            options: {
-              channels: ["email"],
-              api_key: process.env.SENDGRID_API_KEY,
-              from: process.env.SENDGRID_FROM,
-            },
-          },
+          ...(SENDGRID_API_KEY && SENDGRID_FROM_EMAIL
+            ? [
+                {
+                  resolve: "@medusajs/notification-sendgrid",
+                  id: "sendgrid",
+                  options: {
+                    channels: ["email"],
+                    api_key: SENDGRID_API_KEY,
+                    from: SENDGRID_FROM_EMAIL,
+                  },
+                },
+              ]
+            : []),
+          ...(RESEND_API_KEY && RESEND_FROM_EMAIL
+            ? [
+                {
+                  resolve: "./src/modules/email-notifications",
+                  id: "resend",
+                  options: {
+                    channels: ["email"],
+                    api_key: RESEND_API_KEY,
+                    from: RESEND_FROM_EMAIL,
+                  },
+                },
+              ]
+            : []),
         ],
       },
-
-      resolve: "@medusajs/medusa/notification",
-      options: {
-        providers: [
-          {
-            resolve: "./src/modules/email-notifications",
-            id: "resend",
-            options: {
-              channels: ["email"],
-              api_key: process.env.RESEND_API_KEY,
-              from: process.env.RESEND_FROM_EMAIL,
-            },
-          },
-        ],
-      },
-
+    },
+    {
       key: Modules.FILE,
       resolve: "@medusajs/file",
       options: {
@@ -121,45 +123,6 @@ const medusaConfig = {
               redis: {
                 url: REDIS_URL,
               },
-            },
-          },
-        ]
-      : []),
-    ...((SENDGRID_API_KEY && SENDGRID_FROM_EMAIL) ||
-    (RESEND_API_KEY && RESEND_FROM_EMAIL)
-      ? [
-          {
-            key: Modules.NOTIFICATION,
-            resolve: "@medusajs/notification",
-            options: {
-              providers: [
-                ...(SENDGRID_API_KEY && SENDGRID_FROM_EMAIL
-                  ? [
-                      {
-                        resolve: "@medusajs/notification-sendgrid",
-                        id: "sendgrid",
-                        options: {
-                          channels: ["email"],
-                          api_key: SENDGRID_API_KEY,
-                          from: SENDGRID_FROM_EMAIL,
-                        },
-                      },
-                    ]
-                  : []),
-                ...(RESEND_API_KEY && RESEND_FROM_EMAIL
-                  ? [
-                      {
-                        resolve: "./src/modules/email-notifications",
-                        id: "resend",
-                        options: {
-                          channels: ["email"],
-                          api_key: RESEND_API_KEY,
-                          from: RESEND_FROM_EMAIL,
-                        },
-                      },
-                    ]
-                  : []),
-              ],
             },
           },
         ]
